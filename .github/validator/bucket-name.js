be60ebd3fc9b1480ece153ed4199639b4d218891
Stian Sørby <stian.sorby@norges-bank.no>
2026-09-03 10:35:13 +0200

    feat: add validator for input field

---
+++ .github/validator/bucket-name.js
@@ -0,0 +1,24 @@
+/**
+ * Validates if a bucket name follows proper formatting.
+ *
+ * @param {string | string[] | {label: string; required: boolean }} field The input field.
+ *
+ * @returns {Promise<string>} An error message or `"success"`
+ */
+export default async (field) => {
+  const core = await import("@actions/core");
+
+  if (typeof field !== "string") {
+    return "Field type is not supported by validator";
+  }
+
+  if (field.length > 63 || field.length < 3) {
+    return "Bucket name must be between 3 and 63 characters";
+  }
+
+  if (!/^[a-z0-9][a-z0-9-]*[a-z0-9]$/.test(field)) {
+    return "Bucket name must start and end with a lowercase letter or number, and can only contain lowercase letters, numbers, and hyphens";
+  }
+
+  return "success";
+}
